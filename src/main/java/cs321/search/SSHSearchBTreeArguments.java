b40b4b8ecb8559c10aa45987f63f5ad14e2328c0
NoahSewBSU <114342727+NoahSewBSU@users.noreply.github.com>
2024-11-22 16:46:05 +0000

    Added and removed comments for next sprint

--- src/main/java/cs321/search/SSHSearchBTreeArguments.java
+++ src/main/java/cs321/search/SSHSearchBTreeArguments.java
@@ -4,4 +4,8 @@ package cs321.search;
 public class SSHSearchBTreeArguments
 {
 
+    /* Parse arguments and save into object */
+
+    /* Create getters to receive variables from the object created from the arguments */
+
 }
